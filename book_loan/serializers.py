cf2e2c04152bda1903db91512d307a14e74b3a94
Adriano Souza <adrianos924@gmail.com>
2023-05-04 19:40:55 -0400

    fix: deleting the comments

--- book_loan/serializers.py
+++ book_loan/serializers.py
@@ -1,11 +1,8 @@
 from rest_framework import serializers
 from .models import BookLoan
-from book_copy.models import BookCopy
-from datetime import date, timedelta
 
 
 class BookLoanSerializer(serializers.ModelSerializer):
-    # max_return_date = serializers.SerializerMethodField()
 
     class Meta:
         model = BookLoan
@@ -19,24 +16,6 @@ class BookLoanSerializer(serializers.ModelSerializer):
         ]
 
         extra_kwargs = {"book_copy": {"read_only": True}, "user": {"read_only": True}}
-
-    def get_max_return_date(self, obj: BookCopy):
-        # import ipdb
-        # ipdb.set_trace()
-        # devolution_book = date.today() + timedelta(
-        #     int(obj.book_copy.is_avaliable)
-        # )
-
-        # if devolution_book.weekday() == 5:
-        #     devolution_book = date.today() + timedelta(
-        #         int(obj.book_copy.lending_time_limit) + 2
-        #     )
-        # if devolution_book.weekday() == 6:
-        #     devolution_book = date.today() + timedelta(
-        #         int(obj.book_copy.lending_time_limit) + 1
-        #     )
-
-        return date.today()
 
     def create(self, validated_data):
         user = validated_data.pop("user")
