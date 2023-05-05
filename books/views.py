c75508265df01ab5da9a77286ae9b36af3423eb7
Joaovicente985 <joaovicente985@msn.com>
2023-05-05 10:09:07 -0300

    fix: book deletion

--- books/views.py
+++ books/views.py
@@ -1,13 +1,13 @@
 from rest_framework import generics
 from rest_framework_simplejwt.authentication import JWTAuthentication
-from .permissions import IsAdminOrReadOnly
+from .permissions import IsEmployeeOrReadOnly, IsEmployee
 from .models import Book
 from .serializers import BookSerializer
 
 
 class BookView(generics.ListCreateAPIView):
     authentication_classes = [JWTAuthentication]
-    permission_classes = [IsAdminOrReadOnly]
+    permission_classes = [IsEmployeeOrReadOnly]
     queryset = Book.objects.all()
     serializer_class = BookSerializer
 
@@ -19,3 +19,10 @@ class BookDetailView(generics.ListAPIView):
 
     def get_queryset(self):
         return Book.objects.filter(id=self.kwargs.get("book_id"))
+    
+class BookDeleteView(generics.DestroyAPIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsEmployee]
+
+    queryset = Book.objects.all()
+    lookup_url_kwarg = "user_id"
