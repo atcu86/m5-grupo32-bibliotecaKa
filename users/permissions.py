b06e65f4537700e3e3d37aa4f3f893dcf756bddc
Natalia <nataliabadilho@hotmail.com>
2023-05-08 18:20:31 -0300

    feat:patch user

--- users/permissions.py
+++ users/permissions.py
@@ -2,17 +2,13 @@ from rest_framework import permissions
 from rest_framework.views import View, Request
 
 
-class IsEmployee(permissions.BasePermission):
-    def has_permission(self, request: Request, view: View) -> bool:
+class IsAuthorized(permissions.BasePermission):
+    def has_object_permission(self, request: Request, view: View, obj) -> bool:
         if request.method == "POST":
             return True
-        else:
-            return request.user.is_authenticated and request.user.is_superuser
-
-
-class IsEmployeeOrOwner(permissions.BasePermission):
-    def has_object_permission(self, request: Request, view: View, obj) -> bool:
         if request.user.is_authenticated:
+            if request.method == "PATCH":
+                return obj.id == request.user.id
             if request.user.is_employee:
                 return True
             else:
